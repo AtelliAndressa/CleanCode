926cd0778c6b4913e46b7a5ed0bc838cd3fd7e9a
Andressa Atelli <andressaatelli.programador@gmail.com>
2023-10-25 19:11:14 -0300

    3

---
+++ CleanArchMvc.Infra.IoC/DependencyInjection.cs
@@ -0,0 +1,36 @@
+﻿using CleanArchMvc.Application.Interfaces;
+using CleanArchMvc.Application.Mappings;
+using CleanArchMvc.Application.Services;
+using CleanArchMvc.Domain.Interfaces;
+using CleanArchMvc.Infra.Data.Context;
+using CleanArchMvc.Infra.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchMvc.Infra.IoC
+{
+    /// <summary>
+    /// Método de extensão. Registro do contexto, banco de dados, string de conexão, indicando a pasta da migrations que 
+    /// ficará no Infra.Data, registro dos serviços do repository.
+    /// Esse método será usado no projeto WebUI
+    /// </summary>
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            
+            services.AddDbContext<ApplicationDbContext>(options => 
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            services.AddScoped<ICategoryRepository, CategoryRepository>(); 
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<IProductService, ProductService>();
+            services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+
+            return services;
+        }
+    }
+}
